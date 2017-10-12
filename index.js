d2cd503a7578259cf63b528a7323a86f0ddd249e
Nick Casciola <s0948644@monmouth.edu>
2017-10-12 21:57:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function funkyFunction() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var myscope = funcyFunction();
+var myscope = funkyFunction();
 var theFunk = myscope();
 
 //var theFunk = funkyFunction()();
